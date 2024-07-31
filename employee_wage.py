15864271101015f65d6890982071083e734ceafa
GirishNekar <gmnekar18@gmail.com>
2024-07-31 13:13:24 +0530

    [Girsh : Committed Daily wage Program]

--- employee_wage.py
+++ employee_wage.py
@@ -1,11 +1,22 @@
 import random
 
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+
 def welcome_message():
     print("Welcome to Employee Wage Computation Program")
 
 def check_attendance():
     return random.choice(["Present", "Absent"])
 
+def calculate_daily_wage(hours):
+    return hours * WAGE_PER_HOUR
+
 if __name__ == "__main__":
     welcome_message()
-    print(f"Employee is {check_attendance()}")
+    attendance = check_attendance()
+    if attendance == "Present":
+        daily_wage = calculate_daily_wage(FULL_DAY_HOUR)
+        print(f"Daily Wage: {daily_wage}")
+    else:
+        print("No wage, employee is absent")
